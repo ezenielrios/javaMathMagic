ad893c288b69f1ae1a12b0deb377d908e160afa2
Ezeniel Rios <75400864+ezenielrios@users.noreply.github.com>
2022-02-18 08:43:40 -0500

    Add files via upload

---
+++ Magic.java
@@ -0,0 +1,16 @@
+public class Magic {
+	public static void main(String[] args) {
+    int myNumber = 100;
+    //myNumber will be the original number going forward
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    System.out.println(stepSix);
+
+	}
+}
+
+  /*Recreate this project using only two variables: myNumber and      magicNumber. Use your understanding of compound assignment operators to recreate the above program by only manipulating magicNumber.*/ 
